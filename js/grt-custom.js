975eb781e80067a5dcbb3909ec3a4fa8ddd7bc29
grt107 <31504327+grt107@users.noreply.github.com>
2020-01-07 01:18:59 +0100

    Add files via upload

--- js/grt-custom.js
+++ js/grt-custom.js
@@ -2,7 +2,7 @@ jQuery(function ($) {
     $.fn.moveright = function() {
         var mywidth = $(this).width() / 1.5;
         var pos = $(this).scrollLeft() + mywidth;
-        $(this).animate( { scrollLeft: pos }, 600 );
+        $(this).stop().animate( { scrollLeft: pos }, 600 );
     }
 });
 
@@ -10,7 +10,7 @@ jQuery(function ($) {
     $.fn.moveleft = function() {
         var mywidth2 = $(this).width() / 1.5;
         var pos2 = $(this).scrollLeft() - mywidth2;
-        $(this).animate( { scrollLeft: pos2 }, 600 );
+        $(this).stop().animate( { scrollLeft: pos2 }, 600 );
     }
 });
 
