a7dc69a6a09d548f1c7180efdc3d5e91268a8983
grt107 <31504327+grt107@users.noreply.github.com>
2020-01-19 20:03:24 +0100

    Add files via upload

--- js/grt-custom.js
+++ js/grt-custom.js
@@ -1,3 +1,5 @@
+
+// Move Right
 jQuery(function ($) {
     $.fn.moveright = function() {
         var mywidth = $(this).width() / 1.5;
@@ -6,12 +8,20 @@ jQuery(function ($) {
     }
 });
 
+// Move Left
 jQuery(function ($) {
     $.fn.moveleft = function() {
         var mywidth2 = $(this).width() / 1.5;
         var pos2 = $(this).scrollLeft() - mywidth2;
         $(this).stop().animate( { scrollLeft: pos2 }, 600 );
     }
+});
+
+/*------- Menu -------*/
+$('.grt-mobile-button').on('click', function(){
+	$(this).toggleClass("grt-mobile-button-open");
+	$("ul.grt-menu").toggleClass("open-grt-menu ");
+	$("html, body").toggleClass("body-overflow");
 });
 
 /*------- Loader -------*/
