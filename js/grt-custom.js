b0d2ae7cf7ffdb3a28bb7f3241ececfc0e168461
grt107 <31504327+grt107@users.noreply.github.com>
2020-04-25 22:13:01 +0200

    update

--- js/grt-custom.js
+++ js/grt-custom.js
@@ -1,22 +1,3 @@
-
-// Move Right
-jQuery(function ($) {
-    $.fn.moveright = function() {
-        var mywidth = $(this).width() / 1.5;
-        var pos = $(this).scrollLeft() + mywidth;
-        $(this).stop().animate( { scrollLeft: pos }, 600 );
-    }
-});
-
-// Move Left
-jQuery(function ($) {
-    $.fn.moveleft = function() {
-        var mywidth2 = $(this).width() / 1.5;
-        var pos2 = $(this).scrollLeft() - mywidth2;
-        $(this).stop().animate( { scrollLeft: pos2 }, 600 );
-    }
-});
-
 /*------- Menu -------*/
 $('.grt-mobile-button').on('click', function(){
 	$(this).toggleClass("grt-mobile-button-open");
